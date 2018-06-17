d159e37728e555b4fcedea02d14f116013944733
bluesilence <bluesilencemail@126.com>
2018-06-16 22:24:19 -0700

    Study 40% done

--- FBPredictCheckIns/Script/ThirdPlaceSolution.R
+++ FBPredictCheckIns/Script/ThirdPlaceSolution.R
@@ -56,6 +56,70 @@ write.csv(candidate, './Output/candidate.csv', row.names = FALSE)
 rm(temp)
 
 ############################### Step 2 #######################################
+cat("step: 2/5\n")
+
+predict.interval <- 24 * 60 * 7 * 2 # 2 weeks
+
+# Count visits per place per interval
+freq <- data.table(place_id = unique(data.train$place_id))
+for (i in 1:(train.end / predict.interval))
+{
+  temp <- data.train[train.end - predict.interval*i < time & time <= train.end - predict.interval*(i - 1), .N, by = "place_id"]
+  freq <- merge(freq, temp, by = "place_id", all.x = TRUE)
+  colnames(freq)[ncol(freq)] <- paste0("freq_", as.integer(train.end - predict.interval*i), "-", as.integer(train.end - predict.interval*(i-1)))
+}
+freq[is.na(freq)] <- 0
+head(freq)
+
+params <- list("eta" = 0.1, "max_depth" = 6, "min_child_weight" = 100, "objective" = "reg:linear", "eval_metric" = "rmse")
+
+pr.place.table <- freq[, .(place_id)]
+
+total_epoch <- ceiling((test.end - train.end) / predict.interval)
+for (i in 1:total_epoch)
+{
+  cat(paste0("    substep: ", i, "/", total_epoch, "\n"))
+  
+  x.train <- freq[, (i+2):ncol(freq), with = FALSE] # Train on place with visits in all the history except for the latest i intervals
+  y.train <- freq[[2]][apply(x.train, 1, sum) > 0] # Prediction target is the last interval & Filter out intervals without visits
+  x.train <- x.train[apply(x.train, 1, sum) > 0]
+  
+  x.test <- freq[, 2:(ncol(freq) - i), with = FALSE] # Test on all the history except for the earliest i intervals
+  
+  x.train <- as.matrix(x.train) * 1.0
+  x.test <- as.matrix(x.test) * 1.0
+  
+  if (i == 1)
+  {
+    nrounds <- 100
+  }
+  else if (i == 2)
+  {
+    nrounds <- 30
+  }
+  else
+  {
+    nrounds <- 10
+  }
+  
+  set.seed(0)
+  model.xgb <- xgb.train(param = params, data = xgb.DMatrix(x.train, label = y.train), nrounds = nrounds)
+  # xgb requires test data to have the same feature names as the training data
+  colnames(x.test) <- model.xgb[["feature_names"]]
+  
+  temp <- predict(model.xgb, x.test)
+  temp[temp < 0] <- 0
+  temp <- temp / sum(temp)
+  pr.place.table <- cbind(pr.place.table, temp)
+  colnames(pr.place.table)[ncol(pr.place.table)] <- paste0("pr_", as.integer(train.end + predict.interval*(i - 1)), "-", as.integer(train.end + predict.interval*i))
+}
+
+write.csv(pr.place.table, "./Output/pr_place.csv", row.names = FALSE)
+
+rm(freq)
+rm(x.train)
+rm(x.test)
+rm(temp)
 
 ############################### Step 3 #######################################
 
